dd001907c44a475524bf084eea936f051d8727a7
Vaish-coder <147021686+Vaish-coder@users.noreply.github.com>
2024-12-12 14:29:41 +0530

    Delete Complaint-System-MERN-master directory

--- Complaint-System-MERN-master/server/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { signin, signup } from "../controller/user.js";
-
-router.post("/signin", signin);
-router.post("/signup", signup);
-
-export default router;
